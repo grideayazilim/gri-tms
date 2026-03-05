516bbf5783b6370a1f06373d0ad70c91231aca0b
elcinkaya <elcinkaya2005@hotmail.com>
2026-03-05 23:37:57 +0300

    Added users API

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,8 +1,25 @@
 import express from "express";
-// import { ... } from "../controllers/userController.js";
+import {
+    getUsers,
+    updateUser,
+    deleteUser,
+    updateProfile,
+} from "../controllers/userController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
+import { adminMiddleware } from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
-// router.get("__route_yolu__", __gerekliyse_middleware'lar__, __controller_fonksiyonu__);
+//Profil Güncelleme
+router.put("/me", authMiddleware, updateProfile);
+
+//Kullanıcıları Listele (Sadece Admin)
+router.get("/", authMiddleware, adminMiddleware, getUsers);
+
+//Kullanıcı Güncelle (Sadece Admin)
+router.put("/:userId", authMiddleware, adminMiddleware, updateUser);
+
+//Kullanıcı Sil (Sadece Admin)
+router.delete("/:userId", authMiddleware, adminMiddleware, deleteUser);
 
 export default router;

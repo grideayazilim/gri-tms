48d253c3184d0ce9d50a1b785709475884bd4f4a
soztalha <23050111069@aybu.edu.tr>
2026-03-15 01:51:42 +0300

    added TimesheetPage fronted-backend connection + code optimization

--- server/src/routes/timesheetRoutes.js
+++ server/src/routes/timesheetRoutes.js
@@ -1,11 +1,17 @@
 import express from "express";
-import { getTimesheets, createOrUpdateTimesheets, lockPeriod } from "../controllers/timesheetController.js";
+import {
+  getTimesheets,
+  createOrUpdateTimesheets,
+  lockPeriod,
+  getPeriods,
+} from "../controllers/timesheetController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { scopeMiddleware } from "../middlewares/scopeMiddleware.js";
 import { adminMiddleware } from "../middlewares/adminMiddleware.js";
 
 const router = express.Router();
 
+router.get("/periods", authMiddleware, getPeriods);
 router.get("/", authMiddleware, scopeMiddleware, getTimesheets);
 router.post("/", authMiddleware, scopeMiddleware, createOrUpdateTimesheets);
 router.patch("/:periodId/lock", authMiddleware, adminMiddleware, lockPeriod);
